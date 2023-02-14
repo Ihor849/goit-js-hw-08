c11f79cf16eb3cf01329b83834317487e63a3485
Ihor849 <333marchelo333@gmail.com>
2023-02-15 01:57:03 +0200

    закіньчує 2е завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const currentTime = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+// ВАРИАНТ 1
+// player.on('timeupdate', throttle(onPlay, 1000));
+
+// ВАРИАНТ 2
+player.on('play', function (e) {
+  throttle = (onPlay(e), 1000);
+  console.log(seconds);
+});
+
+function onPlay({ seconds }) {
+  localStorage.setItem(currentTime, seconds);
+}
+
+function setStopTime() {
+  player.setCurrentTime(localStorage.getItem(currentTime));
+}
+setStopTime();
